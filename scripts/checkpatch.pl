bff0bc007eb6f8f46980c4f87baa1e671cb1fad4
Alberto Escolar Piedras <alpi@oticon.com>
2019-08-12 13:15:20 +0200

    (revert me) checkpatch.pl: Do not require CODEOWNERS
    checkpatch.pl was made to require CODEOWNERS being in the
    root folder for it to accept it as a Zephyr tree.
    But we renamed it in this branch.
    => Remove this requirement in this branch
    
    (Re-applying in the le-audio branch the commit Alberto did for the
    ble-audio branch - Asbjørn)
    
    Signed-off-by: Alberto Escolar Piedras <alpi@oticon.com>
    Signed-off-by: Asbjørn Sæbø <asbjorn.sabo@nordicsemi.no>

--- scripts/checkpatch.pl
+++ scripts/checkpatch.pl
@@ -1118,7 +1118,7 @@ sub top_of_kernel_tree {
 	my ($root) = @_;
 
 	my @tree_check = (
-		"LICENSE", "CODEOWNERS", "Kconfig", "Makefile",
+		"LICENSE", "Kconfig", "Makefile",
 		"README.rst", "doc", "arch", "include", "drivers",
 		"boards", "kernel", "lib", "scripts",
 	);
