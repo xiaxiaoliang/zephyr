e6e47c5d636b1fcb5183ea5b824aed9268ede927
Emil Gydesen <emil.gydesen@nordicsemi.no>
2020-10-16 15:41:34 +0200

    Bluetooth: Audio: Add initial bsim test directory
    Adds a new BabbleSim (BSIM) test directory for Bluetooth
    Audio tests, that can be expanded with tests.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

---
+++ tests/bluetooth/bsim_bt/bsim_test_audio/src/main.c
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "bstests.h"
+
+bst_test_install_t test_installers[] = {
+	NULL
+};
+
+void main(void)
+{
+	bst_main();
+}
